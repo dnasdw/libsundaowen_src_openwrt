e4df91788d2a56b28d6d9f12c98390a2449bcc1f
Sun Daowen <sundaowen@gmail.com>
2018-10-17 14:36:04 +0800

    copy from libsundaowen_src

---
+++ sdw.h
@@ -0,0 +1,6 @@
+#ifndef LIBSUNDAOWEN_SDW_H_
+#define LIBSUNDAOWEN_SDW_H_
+
+#include "sdw_type.h"
+
+#endif	// LIBSUNDAOWEN_SDW_H_
---
+++ sdw_type.h
@@ -0,0 +1,6 @@
+#ifndef LIBSUNDAOWEN_SDW_TYPE_H_
+#define LIBSUNDAOWEN_SDW_TYPE_H_
+
+#include <cstdio>
+
+#endif	// LIBSUNDAOWEN_SDW_TYPE_H_
